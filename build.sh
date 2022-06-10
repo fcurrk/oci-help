f95025d96f427fed1a429cc253c31eb46fde6420
fcurrk <48197714+fcurrk@users.noreply.github.com>
2022-06-10 12:01:32 +0800

    update

--- build.sh
+++ build.sh
@@ -4,12 +4,19 @@
 BUILD() {
   cd oci-help
   go get github.com/fatih/color
+  go get github.com/oracle/oci-go-sdk/v65/common
+  go get github.com/oracle/oci-go-sdk/v65/core
+  go get github.com/oracle/oci-go-sdk/v65/example/helpers
+  go get github.com/oracle/oci-go-sdk/v65/identity
   appName="oci-help"
 
   rm -rf .git/
   xgo -targets=linux/amd64,windows/amd64,linux/arm64 -out "$appName" -ldflags="-X main.Version=2.2.1" -tags=jsoniter .
   mkdir -p "build"
   mv oci-help* build
+  mv go* build
+
+
  cd build
  upx -9 ./oci-help-linux*
  upx -9 ./oci-help-windows*
