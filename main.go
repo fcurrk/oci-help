aa3e056e6f4b1f2e2a6ca7e1025139bf7e81e0a3
fcurrk <48197714+fcurrk@users.noreply.github.com>
2022-06-11 10:07:26 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1151,6 +1151,10 @@ func LaunchInstances(ads []identity.AvailabilityDomain) (sum, num int32) {
 		if err != nil {
 			printlnErr("消息提醒发送失败", err.Error())
 		}
+		if err2 != nil {
+                        fmt.Sprintf("SUM %d  TEXT %s",err2,err2)
+			printlnErr("WX消息提醒发送失败", err2.Error())
+		}
 	}
 
 	for pos < sum {
